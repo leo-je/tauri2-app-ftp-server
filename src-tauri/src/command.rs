28ee220be2dbf689868f298afcc4d70af3bbd85c
火龙果 <405192400@qq.com>
2025-02-16 11:04:47 +0800

    commit:优化代码

---
+++ src-tauri/src/command.rs
@@ -0,0 +1,76 @@
+use std::sync::{Arc, Mutex};
+
+use crate::ftpworker::FtpWorker;
+
+// 添加输入验证函数
+pub fn validate_path(path: &str) -> bool {
+    // 实现路径验证逻辑
+    !path.is_empty()
+}
+
+pub fn validate_port(port: &str) -> bool {
+    // 实现端口验证逻辑
+    port.parse::<u16>().is_ok()
+}
+
+#[tauri::command]
+pub fn start_ftp_server(
+    state: tauri::State<'_, Arc<Mutex<FtpWorker>>>,
+    path: String,
+    port: String,
+) -> Result<String, String> {
+    // 验证输入参数
+    if !validate_path(&path) || !validate_port(&port) {
+        return Err("无效的路径或端口".to_string());
+    }
+
+    let mut worker = match state.lock() {
+        Ok(guard) => guard,
+        Err(e) => {
+            // error!("无法获取锁: {}", e);
+            return Err(format!("无法获取锁: {}", e));
+        }
+    };
+
+    if worker.is_running() {
+        // info!("FTP 服务已启动");
+        return Ok("服务已启动".to_string());
+    }
+
+    // 设置路径和端口
+    worker.set(path.clone(), port.clone());
+
+    // 启动 FTP 服务
+    match worker.start() {
+        Ok(_) => {
+            // info!("FTP 服务启动成功");
+            Ok("服务已启动".to_string())
+        }
+        Err(e) => {
+            // error!("FTP 服务启动失败: {}", e);
+            Err(format!("服务启动失败: {}", e))
+        }
+    }
+}
+
+#[tauri::command]
+pub fn stop_ftp_server(state: tauri::State<'_, Arc<Mutex<FtpWorker>>>) -> Result<String, String> {
+    let mut worker = match state.lock() {
+        Ok(guard) => guard,
+        Err(e) => {
+            // error!("无法获取锁: {}", e);
+            return Err(format!("无法获取锁: {}", e));
+        }
+    };
+
+    match worker.stop() {
+        Ok(_) => {
+            // info!("FTP 服务已停止");
+            Ok("服务已停止".to_string())
+        }
+        Err(e) => {
+            // error!("FTP 服务停止失败: {}", e);
+            Err(format!("FTP 服务停止失败: {}", e))
+        }
+    }
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,82 +4,8 @@ use std::sync::{Arc, Mutex};
 use tauri::Manager;
 use tauri_plugin_log::{Target, TargetKind};
 
+pub mod command;
 pub mod ftpworker;
-use crate::ftpworker::FtpWorker;
-
-
-// 添加输入验证函数
-fn validate_path(path: &str) -> bool {
-    // 实现路径验证逻辑
-    !path.is_empty()
-}
-
-fn validate_port(port: &str) -> bool {
-    // 实现端口验证逻辑
-    port.parse::<u16>().is_ok()
-}
-
-#[tauri::command]
-fn start_ftp_server(
-    state: tauri::State<'_, Arc<Mutex<FtpWorker>>>,
-    path: String,
-    port: String,
-) -> Result<String, String> {
-    // 验证输入参数
-    if !validate_path(&path) || !validate_port(&port) {
-        return Err("无效的路径或端口".to_string());
-    }
-
-    let mut worker = match state.lock() {
-        Ok(guard) => guard,
-        Err(e) => {
-            // error!("无法获取锁: {}", e);
-            return Err(format!("无法获取锁: {}", e));
-        }
-    };
-
-    if worker.is_running() {
-        // info!("FTP 服务已启动");
-        return Ok("服务已启动".to_string());
-    }
-
-    // 设置路径和端口
-    worker.set(path.clone(), port.clone());
-
-    // 启动 FTP 服务
-    match worker.start() {
-        Ok(_) => {
-            // info!("FTP 服务启动成功");
-            Ok("服务已启动".to_string())
-        }
-        Err(e) => {
-            // error!("FTP 服务启动失败: {}", e);
-            Err(format!("服务启动失败: {}", e))
-        }
-    }
-}
-
-#[tauri::command]
-fn stop_ftp_server(state: tauri::State<'_, Arc<Mutex<FtpWorker>>>) -> Result<String, String> {
-    let mut worker = match state.lock() {
-        Ok(guard) => guard,
-        Err(e) => {
-            // error!("无法获取锁: {}", e);
-          return Err(format!("无法获取锁: {}", e));
-        }
-    };
-
-    match worker.stop() {
-        Ok(_) => {
-            // info!("FTP 服务已停止");
-            Ok("服务已停止".to_string())
-        },
-        Err(e) => {
-            // error!("FTP 服务停止失败: {}", e);
-            Err(format!("FTP 服务停止失败: {}", e))
-        }
-    }
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -87,7 +13,7 @@ pub fn run() {
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .setup(move |app| {
-            let worker = Arc::new(Mutex::new(FtpWorker::new()));
+            let worker = Arc::new(Mutex::new(ftpworker::FtpWorker::new()));
             app.manage(worker);
             Ok(())
         })
@@ -95,7 +21,10 @@ pub fn run() {
         .plugin(tauri_plugin_shellx::init(true))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![start_ftp_server, stop_ftp_server,])
+        .invoke_handler(tauri::generate_handler![
+            command::start_ftp_server,
+            command::stop_ftp_server,
+        ])
         .plugin(
             tauri_plugin_log::Builder::new()
                 .targets([
